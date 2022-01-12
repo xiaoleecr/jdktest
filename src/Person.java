cb0bed222a54af00218cae19caf940e18fdfa681
licongrui <xiaoleecr@live.cn>
2022-01-12 16:49:13 +0800

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,34 @@
+/**
+ * @ClassName Person
+ * @Description
+ * @Author licongrui
+ * @Date 2022/1/4
+ */
+public class Person {
+    private String name;
+    private Integer age;
+
+    Person() {
+    }
+
+    Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
---
+++ src/SdfTest.java
@@ -0,0 +1,32 @@
+import com.sun.deploy.util.StringUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @ClassName SdfTest
+ * @Description
+ * @Author licongrui
+ * @Date 2021/12/30
+ */
+public class SdfTest {
+    public static void main(String[] args) {
+        List<Person> pList = new ArrayList<>();
+        pList.add(new Person("Jack", 19));
+        pList.add(new Person("Mike", 25));
+        pList.add(new Person("Tom", 27));
+        pList.add(new Person("Max", 27));
+        pList.add(new Person());
+        List<Person> collect = pList.stream().filter(p -> p.getAge() != null && p.getName().equals("jean")).map(p -> new Person(p.getName(),p.getAge())).distinct().collect(Collectors.toList());
+        String names = pList.stream().filter(p-> null != p.getName()).map(p -> p.getName()).collect(Collectors.joining(","));
+
+//        String begTime = "2021-01-0";
+//        begTime = begTime.substring(0,10);
+//        System.out.println(begTime);
+    }
+}
